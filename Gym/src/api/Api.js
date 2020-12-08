494b02520b80cd80c3e50130a9338a7b697c4f24
AlessandroS94 <alessandro.sallese@student.univaq.it>
2020-12-08 10:08:40 +0100

    Update Api.js

--- Gym/src/api/Api.js
+++ Gym/src/api/Api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import firebase, {firestore} from '../firebase/firebase';
 
-const URL_VIDEO = 'https://www.googleapis.com/youtube/v3/playlistItems?part=id%2Csnippet&maxResults=10&playlistId=PLA21FAE7C8EE25330&key=AIzaSyAYVriqGxTsWgVFx7YG6czyMAOtp7IrUzI';
+const URL_VIDEO = 'https://www.googleapis.com/youtube/v3/playlistItems?part=id%2Csnippet&maxResults=10&playlistId=PLA21FAE7C8EE25330&key=';
 
 export const getVideos = function () {
     return axios.get(URL_VIDEO).
